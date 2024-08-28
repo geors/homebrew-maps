05528d7e02095a157d59d9f233405096249f6c22
Vyacheslav Rakhinskiy <v.rakhinskiy@vk.team>
2024-08-28 18:54:29 +0300

    Switch fmt@10 to build fom source

--- Formula/fmt@10.rb
+++ Formula/fmt@10.rb
@@ -1,21 +1,8 @@
 class FmtAT10 < Formula
   desc "Open-source formatting library for C++"
   homepage "https://fmt.dev/"
-  url "https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz"
-  sha256 "1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811"
+  url "https://github.com/fmtlib/fmt.git", :using => :git, :tag => "10.2.1"
   license "MIT"
-  revision 1
-  head "https://github.com/fmtlib/fmt.git", branch: "master"
-
-  bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "666aae4a2019ed7957e96ed6e6d182db676a2088a7b1646bb0a1ad48809fe571"
-    sha256 cellar: :any,                 arm64_ventura:  "3316fbda0bed3dd713f2045c3fefa14fead6fe1a11c58c55262dee93a40e2bda"
-    sha256 cellar: :any,                 arm64_monterey: "80d218961620251763fcaac82ae04ce2d604ad6c86cd19ae0320c54ba7b1b0f4"
-    sha256 cellar: :any,                 sonoma:         "c193bef5f45e097a20c2b622dee0347dc76a8e1fe49de12297f5803ab2b2f977"
-    sha256 cellar: :any,                 ventura:        "772fed0ecbf537c7a55d768b380363324e815c7805dfc4322749abdc4ebdeb9e"
-    sha256 cellar: :any,                 monterey:       "5ed3d39677cf4bec72c05cd8ae62adf102b38ba82dcdea545f41f3a1dea39443"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd75f94114953993f1a36280918192dae764319c3f8e2f54e875d9f8266848f0"
-  end
 
   depends_on "cmake" => :build
 
